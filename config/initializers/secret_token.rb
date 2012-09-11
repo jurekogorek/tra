80335cce362b90a51b84121757d952eb5c8d5409
jurekogorek <jerzy.czepiel@gmail.com>
2012-09-11 23:05:10 +0200

    dupa

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tra::Application.config.secret_token = '8b09209d499f8c8312b8cedf3299522b24416ef71cc0d231db54e1b3f91b2eabfbb840ac852b4e48762614b49f3667ba3690279212b0469d2995f21614efe2c3'
